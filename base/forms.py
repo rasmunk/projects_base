965952e560d25da5ec03541731fe58c00b97509f
Rasmus Munk <munk1@live.dk>
2020-04-19 10:47:54 +0200

    Fix Style

--- base/forms.py
+++ base/forms.py
@@ -11,16 +11,13 @@ class FormManager:
     def __init__(self, default_class=None, default_module=None, custom_key=None):
         # Add the package config form class if defined
         if default_class and default_module:
-            self.add_module_class(
-                default_module, default_class, custom_key=custom_key
-            )
+            self.add_module_class(default_module, default_class, custom_key=custom_key)
 
     def add_module_class(self, module_name, class_name, custom_key=None):
         if custom_key:
             self.forms[custom_key] = [module_name, class_name]
         else:
             self.forms[class_name] = [module_name, class_name]
-
 
     def get_form_class(self, key):
         module_, class_ = self.forms[key]
